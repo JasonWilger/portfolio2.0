fbd9cebe4530a1e146e0ffbc9d8715d86c4452a1
JasonWilger <jkw500@gmail.com>
2021-02-24 15:02:52 -0500

    ready for deploy

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 1000;
 
+const app = express();
 app.use(express.static('public'))
 
-
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => console.log('Server running on port 1000!'));
